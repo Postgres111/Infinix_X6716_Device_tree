ef691431f26ca43ce0275799491332642b5cd794
Postgres111 <klepikovpavel2@gmail.com>
2025-12-06 17:17:59 +0300

    Update omni_Infinix_X6716.mk

--- omni_Infinix_X6716.mk
+++ omni_Infinix_X6716.mk
@@ -13,7 +13,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from Infinix-X6716 device
-$(call inherit-product, device/infinix/Infinix-X6716/device.mk)
+$(call inherit-product, device/infinix/Infinix_X6716/device.mk)
 
 PRODUCT_DEVICE := Infinix_X6716
 PRODUCT_NAME := omni_Infinix_X6716
